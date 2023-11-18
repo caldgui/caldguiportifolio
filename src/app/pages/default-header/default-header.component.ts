1d98c1fc9b4077689ad954895b72a024bbe165d3
Guilherme Teixeira Caldana <guicaldana7@gmail.com>
2023-11-18 03:05:33 -0300

    Tentando ajeitar responsividade da tela inicial

--- src/app/pages/default-header/default-header.component.ts
+++ src/app/pages/default-header/default-header.component.ts
@@ -2,7 +2,8 @@ import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-default-header',
-  templateUrl: './default-header.component.html'
+  templateUrl: './default-header.component.html',
+  styleUrls: ['./default-header.component.scss']
 })
 export class DefaultHeaderComponent {
     isCollapsed = true;
