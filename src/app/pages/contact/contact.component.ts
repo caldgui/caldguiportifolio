b7afe65da250c4125c7c7e78ad701c54b961d3fd
Guilherme Teixeira Caldana <guicaldana7@gmail.com>
2023-11-20 09:38:36 -0300

    Fazendo página com formulario de contato

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -3,6 +3,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
+  styleUrls: ["contact.component.scss"]
 })
 export class ContactComponent implements OnInit, OnDestroy {
   isCollapsed = true;
